95a50f2f9a2f748e06d90b04256e6c8ed534d371
rahook92 <rahook92@gmail.com>
2019-11-10 18:44:02 -0800

    Add files via upload

---
+++ blog_project/public/js/index.js
@@ -0,0 +1,10 @@
+
+
+
+// var books = document.querySelector('.books');
+// books.addEventListener('click', ()=>{
+//     var xml = new XMLHttpRequest();
+//     xml.open('GET', './books.html', true);
+//     xml.send();
+//     console.log(xml.responseText);
+// })
